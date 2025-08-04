6c1ee6f8c28b29f635ec63e8c9408de6e7fc3b3d
MD. Ahasanul Islam Nafim <nafim191119@gmail.com>
2025-08-04 11:58:30 +0600

    updated

--- src/pages/Home/Affiliations/Affiliations.jsx
+++ src/pages/Home/Affiliations/Affiliations.jsx
@@ -6,7 +6,7 @@ const Affiliations = () => {
     return (
         <div>
             <div>
-                <Sectiontitle heading={"AFFILIATIONS"} subHeading={"Proudly Associated with IEEE and Its Global Network"}></SectionTitle>
+                <Sectiontitle heading={"AFFILIATIONS"} subHeading={"Proudly Associated with IEEE and Its Global Network"}></Sectiontitle>
                 <img className='mx-auto pb-8 h-56' src="https://i.ibb.co/TMt9ZTHm/logo.png" alt="MIST logo" />
             </div>
         </div>
